b41d3f32183d4cb0a54fdb39ed0a9aae2e2bda6d
mdesanic21 <mdesanic21@foi.hr>
2024-01-25 13:13:47 +0100

    Created User Control for managing clients. Created design for it.

---
+++ Software/ManageIT/ManageIT/MainActivity/UcClients.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManageIT.MainActivity
+{
+    /// <summary>
+    /// Interaction logic for ClientView.xaml
+    /// </summary>
+    public partial class UcClients : UserControl
+    {
+        public UcClients()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearchClients_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClearClients_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddClient_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUpdateClient_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRemoveClient_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
