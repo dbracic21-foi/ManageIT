e0b16fc87ba4fe04cb09623015eeb9b3748072fd
dbracic21-foi <dbracic21@student.foi.hr>
2024-01-25 11:17:46 +0100

    Made design for login Add data into worker and worker type Create initial repository in DAL

---
+++ Software/ManageIT/DataAccessLayer/Repository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public abstract class Repository<T> : IDisposable where T : class
+    {
+        protected ManageITModel Context { get; set; }
+        protected DbSet<T> Entities { get; set; }
+        public Repository()
+        {
+            Context = new ManageITModel();
+            Entities = Context.Set<T>();
+        }
+        public virtual IQueryable<T> GetAll()
+        {
+            var query = from x in Entities
+                        select x;
+            return query;
+        }
+        public virtual void Dispose()
+        {
+            Context.Dispose();
+        }   
+    }
+}
