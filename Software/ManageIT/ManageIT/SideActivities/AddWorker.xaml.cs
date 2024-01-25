13e9f60608bc4f338b7fa700a36efd19cb3654c0
dbracic21-foi <dbracic21@student.foi.hr>
2024-01-25 21:37:45 +0100

    Made initial design for new worker

---
+++ Software/ManageIT/ManageIT/SideActivities/AddWorker.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManageIT.SideActivities
+{
+    /// <summary>
+    /// Interaction logic for AddWorker.xaml
+    /// </summary>
+    public partial class AddWorker : UserControl
+    {
+        public AddWorker()
+        {
+            InitializeComponent();
+        }
+    }
+}
