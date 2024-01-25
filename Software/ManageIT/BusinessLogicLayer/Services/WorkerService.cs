5924ce3e26616adc9edb8fa65c8ff6198553487a
dbracic21-foi <dbracic21@student.foi.hr>
2024-01-25 11:43:13 +0100

    Made Worker Repository Made Worker Service

---
+++ Software/ManageIT/BusinessLogicLayer/Services/WorkerService.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Repositories;
+using EntitiesLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services
+{
+    public class WorkerService
+    {       
+        public IQueryable<Worker> GetWorkerByEmailAndPassword(string email, string password)
+        {
+            using (var workerRepo = new WorkerRepo())
+            {
+                return workerRepo.GetWorkerByEmailAndPassword(email, password);
+            }
+        }
+    }
+}
